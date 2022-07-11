2795600a94438660173f0ea0ed1c5fde9542d747
THARUN01 <v.tharuniitm001@gmail.com>
2022-07-11 22:22:45 +0530

    Starter files

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <div>Welcome to Summer School 2022</div>;
+}
+
+export default App;
---
+++ client/src/shared/data.js
@@ -0,0 +1,503 @@
+export const BoardCell = [
+  [
+    {
+      id: 100,
+      player: [],
+    },
+    {
+      id: 99,
+      move: 78,
+      player: [],
+    },
+    {
+      id: 98,
+      player: [],
+    },
+    {
+      id: 97,
+      player: [],
+    },
+    {
+      id: 96,
+      player: [],
+    },
+    {
+      id: 95,
+      move: 75,
+      player: [],
+    },
+    {
+      id: 94,
+      player: [],
+    },
+    {
+      id: 93,
+      player: [],
+      move: 73,
+    },
+    {
+      id: 92,
+      player: [],
+    },
+    {
+      id: 91,
+      player: [],
+    },
+  ],
+  [
+    {
+      id: 81,
+      player: [],
+    },
+    {
+      id: 82,
+      player: [],
+    },
+    {
+      id: 83,
+      player: [],
+    },
+    {
+      id: 84,
+      player: [],
+    },
+    {
+      id: 85,
+      player: [],
+    },
+
+    {
+      id: 86,
+      player: [],
+    },
+    {
+      id: 87,
+      player: [],
+      move: 24,
+    },
+    {
+      id: 88,
+      player: [],
+    },
+    {
+      id: 89,
+      player: [],
+    },
+    {
+      id: 90,
+      player: [],
+    },
+  ],
+  [
+    {
+      id: 80,
+      player: [],
+    },
+    {
+      id: 79,
+      player: [],
+    },
+    {
+      id: 78,
+      player: [],
+    },
+    {
+      id: 77,
+      player: [],
+    },
+    {
+      id: 76,
+      player: [],
+    },
+    {
+      id: 75,
+      player: [],
+    },
+    {
+      id: 74,
+      player: [],
+    },
+    {
+      id: 73,
+      player: [],
+    },
+    {
+      id: 72,
+
+      player: [],
+    },
+    {
+      id: 71,
+      player: [],
+      move: 91,
+    },
+  ],
+  [
+    {
+      id: 61,
+      player: [],
+    },
+    {
+      id: 62,
+      player: [],
+      move: 19,
+    },
+    {
+      id: 63,
+      player: [],
+      move: 81,
+    },
+    {
+      id: 64,
+      player: [],
+      move: 60,
+    },
+    {
+      id: 65,
+      player: [],
+    },
+
+    {
+      id: 66,
+      player: [],
+    },
+    {
+      id: 67,
+      player: [],
+    },
+    {
+      id: 68,
+      player: [],
+    },
+    {
+      id: 69,
+      player: [],
+    },
+    {
+      id: 70,
+      player: [],
+    },
+  ],
+  [
+    {
+      id: 60,
+      player: [],
+    },
+    {
+      id: 59,
+      player: [],
+    },
+    {
+      id: 58,
+      player: [],
+    },
+    {
+      id: 57,
+      player: [],
+    },
+    {
+      id: 56,
+      player: [],
+    },
+    {
+      id: 55,
+      player: [],
+    },
+    {
+      id: 54,
+      player: [],
+      move: 34,
+    },
+    {
+      id: 53,
+      player: [],
+    },
+    {
+      id: 52,
+      player: [],
+    },
+    {
+      id: 51,
+      player: [],
+    },
+  ],
+  [
+    {
+      id: 41,
+      player: [],
+    },
+    {
+      id: 42,
+      player: [],
+    },
+    {
+      id: 43,
+      player: [],
+    },
+    {
+      id: 44,
+      player: [],
+    },
+    {
+      id: 45,
+      player: [],
+    },
+
+    {
+      id: 46,
+      player: [],
+    },
+    {
+      id: 47,
+      player: [],
+    },
+    {
+      id: 48,
+      player: [],
+    },
+    {
+      id: 49,
+      player: [],
+    },
+    {
+      id: 50,
+      player: [],
+    },
+  ],
+  [
+    {
+      id: 40,
+      player: [],
+      move: 59,
+    },
+    {
+      id: 39,
+      player: [],
+    },
+    {
+      id: 38,
+      player: [],
+    },
+    {
+      id: 37,
+      player: [],
+    },
+    {
+      id: 36,
+      player: [],
+    },
+    {
+      id: 35,
+      player: [],
+    },
+    {
+      id: 34,
+      player: [],
+    },
+    {
+      id: 33,
+      player: [],
+    },
+    {
+      id: 32,
+      player: [],
+    },
+    {
+      id: 31,
+      player: [],
+    },
+  ],
+  [
+    {
+      id: 21,
+      player: [],
+    },
+    {
+      id: 22,
+      player: [],
+    },
+    {
+      id: 23,
+      player: [],
+    },
+    {
+      id: 24,
+      player: [],
+    },
+    {
+      id: 25,
+      player: [],
+    },
+
+    {
+      id: 26,
+      player: [],
+    },
+    {
+      id: 27,
+      player: [],
+    },
+    {
+      id: 28,
+      player: [],
+      move: 38,
+    },
+    {
+      id: 29,
+      player: [],
+    },
+    {
+      id: 30,
+      player: [],
+    },
+  ],
+  [
+    {
+      id: 20,
+      player: [],
+    },
+    {
+      id: 19,
+      player: [],
+    },
+    {
+      id: 18,
+      player: [],
+    },
+    {
+      id: 17,
+      player: [],
+      move: 7,
+    },
+    {
+      id: 16,
+      player: [],
+    },
+    {
+      id: 15,
+      player: [],
+    },
+    {
+      id: 14,
+      player: [],
+    },
+    {
+      id: 13,
+      player: [],
+    },
+    {
+      id: 12,
+      player: [],
+    },
+    {
+      id: 11,
+      player: [],
+    },
+  ],
+  [
+    {
+      id: 1,
+      player: [],
+    },
+    {
+      id: 2,
+      player: [],
+    },
+    {
+      id: 3,
+      player: [],
+    },
+    {
+      id: 4,
+      player: [],
+      move: 14,
+    },
+    {
+      id: 5,
+      player: [],
+    },
+
+    {
+      id: 6,
+      player: [],
+    },
+    {
+      id: 7,
+      player: [],
+    },
+    {
+      id: 8,
+      player: [],
+    },
+    {
+      id: 9,
+      player: [],
+      move: 30,
+    },
+    {
+      id: 10,
+      player: [],
+    },
+  ],
+];
+export const colorCode = ["#dc3545", "#198754"];
+export const LadderAndSnakes = [
+  {
+    cell: 4,
+    move: 14,
+  },
+  {
+    cell: 9,
+    move: 30,
+  },
+  {
+    cell: 17,
+    move: 7,
+  },
+  {
+    cell: 20,
+    move: 38,
+  },
+  {
+    cell: 40,
+    move: 59,
+  },
+  {
+    cell: 54,
+    move: 34,
+  },
+  {
+    cell: 62,
+    move: 19,
+  },
+  {
+    cell: 63,
+    move: 81,
+  },
+  {
+    cell: 64,
+    move: 60,
+  },
+  {
+    cell: 71,
+    move: 91,
+  },
+  {
+    cell: 87,
+    move: 24,
+  },
+  {
+    cell: 93,
+    move: 73,
+  },
+  {
+    cell: 95,
+    move: 75,
+  },
+  {
+    cell: 99,
+    move: 78,
+  },
+];
+export const ExtraMove = [6];
+export const NonExtraMove = [1, 2, 3, 4, 5];
